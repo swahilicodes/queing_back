651aaebff7435369a315de1e854ea39b18c65855
unknown <leebabou2@gmail.com>
2024-10-25 19:19:37 +0300

    45retete

--- data/dokta.js
+++ data/dokta.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { Dokta, User, Counter, Patient } = require('../models/index')
+const { Dokta, User, Counter, Patient, Ticket } = require('../models/index')
 const router = express.Router();
 const { Op } = require('sequelize')
 const bcrypt = require("bcryptjs")
@@ -105,15 +105,6 @@ router.get('/get_free_doktas', async (req, res) => {
         res.status(500).json({ error: err });
     }
 });
-router.get('/get_all_doctors', async (req, res) => {
-    try {
-        const curr = await Doctor.findAll()
-        res.json(curr);
-    } catch (err) {
-        //next({error: err})
-        res.status(500).json({ error: err });
-    }
-});
 router.put('/delete_dokta/:id', async (req, res) => {
     const id = req.params.id
     try {
@@ -135,143 +126,33 @@ router.put('/delete_dokta/:id', async (req, res) => {
         res.status(500).json({ error: err });
     }
 });
-router.put('/edit_counter/:id', async (req, res) => {
-    const id = req.params.id
-    const newData = req.body
-    console.log(newData)
+// Finish Patient
+router.post('/finish_patient', async (req, res) => {
+    const {doctor_id, patient_id} = req.body
     try {
-        if(newData.name.trim()===""){
-            return res.status(404).json({ error: 'name is empty' }); 
+        const doc = await Dokta.findOne({
+            where: {phone: doctor_id}
+        });
+        const tic = await Ticket.findOne({
+            where: {mr_no: patient_id, stage: "clinic"}
+        });
+        if (!doc) {
+        return res.status(404).json({ error: 'doctor not found' });
+        }else if (!tic){
+            return res.status(404).json({ error: 'patient not found' });
         }else{
-            const service = await Counter.findByPk(id);
-            if (!service) {
-            return res.status(404).json({ error: 'service not found' });
-            }
-            await service.update(newData);
-            //res.status(204).end();
-            res.json(service);   
+            tic.update({
+                stage: "out",
+            })
+            doc.update({
+                current_patient: null
+            })
+            res.json(doc)
         }
     } catch (err) {
         //next({error: err})
         res.status(500).json({ error: err });
     }
 });
-router.put('/edit_doctor/:id', async (req, res) => {
-    const id = req.params.id
-    const newData = req.body
-    try {
-        if(!newData.fields.name.trim()===""){
-            return res.status(404).json({ error: 'name is empty' }); 
-        }else{
-            const service = await Doctor.findByPk(id);
-            if (!service) {
-            return res.status(404).json({ error: 'service not found' });
-            }else{
-                const user = await User.findOne({
-                    where: {phone: service.phone}
-                });
-                const counter = await Counter.findOne({
-                    where: {name: newData.fields.service}
-                });
-                if(!user || ! counter){
-                    return res.status(404).json({ error: 'user not found' });  
-                }else{
-                    const pass = await bcrypt.hash(newData.fields.phone,6)
-                    if(newData.fields.phone === user.phone){
-                        await service.update({
-                            name: newData.fields.name,
-                            phone: newData.fields.phone,
-                            service: newData.fields.service,
-                            room: newData.fields.service !=="clinic"?counter.namba:newData.fields.counter,
-                            role:newData.fields.service==="meds"?"medical_recorder":newData.fields.service==="accounts"?"accountant":newData.fields.service==="payment"?"cashier":"doctor",
-                        });
-                        await user.update({
-                            name: newData.fields.name,
-                            phone: newData.fields.phone,
-                            service: newData.fields.service,
-                            counter: newData.fields.service !=="clinic"?counter.namba:newData.fields.counter,
-                            role:newData.fields.service==="meds"?"medical_recorder":newData.fields.service==="accounts"?"accountant":newData.fields.service==="payment"?"cashier":"doctor",
-                        });
-                        res.json(service);
-                    }else{
-                        await service.update({
-                            name: newData.fields.name,
-                            phone: newData.fields.phone,
-                            service: newData.fields.service,
-                            room: newData.fields.service !=="clinic"?counter.namba:newData.fields.counter,
-                            password: pass,
-                            role:newData.fields.service==="meds"?"medical_recorder":newData.fields.service==="accounts"?"accountant":newData.fields.service==="payment"?"cashier":"doctor",
-                        });
-                        await user.update({
-                            name: newData.fields.name,
-                            phone: newData.fields.phone,
-                            service: newData.fields.service,
-                            counter: newData.fields.service !=="clinic"?counter.namba:newData.fields.counter,
-                            password: pass,
-                            role:newData.fields.service==="meds"?"medical_recorder":newData.fields.service==="accounts"?"accountant":newData.fields.service==="payment"?"cashier":"doctor",
-                        });
-                        //res.status(204).end();
-                        res.json(service);
-                    }
-                }
-            }   
-        }
-    } catch (err) {
-        //next({error: err})
-        res.status(500).json({ error: err });
-    }
-});
-
-// get doctor patient
-router.get('/doctor_patient', async (req, res) => {
-    const {phone} = req.query
-    try{
-        if(!phone){
-            return res.status(400).json({ error: 'phone is required' });
-        }else{
-            const doctor = await Doctor.findOne({
-                where: {phone}
-            })
-            if(doctor){
-                const user = await Patient.findOne({
-                    where: {mr_no: doctor.dataValues.current_patient,status: "waiting",stage:"clinic"}
-                })
-                res.json(user)
-            }
-        }
-    }catch(err) {
-        res.status(500).json({ error: err }); 
-    }
-})
-// get doctor patient
-router.get('/finish_doctor_patient', async (req, res) => {
-    const {phone} = req.query
-    console.log('phone is ',phone)
-    try{
-        if(!phone){
-            return res.status(400).json({ error: 'phone is required' });
-        }else{
-            const doctor = await Doctor.findOne({
-                where: {phone}
-            })
-            if(doctor){
-                const pat = await Patient.findOne({
-                    where: {mr_no: doctor.dataValues.current_patient,stage:"clinic",status:"waiting"}
-                })
-                if(pat){
-                    doctor.update({
-                        current_patient: null
-                    })
-                    pat.update({
-                        status:"done"
-                    })
-                    res.json(doctor)
-                }
-            }
-        }
-    }catch(err) {
-        res.status(500).json({ error: err }); 
-    }
-})
 
 module.exports = router;
