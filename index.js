5bceabe8b8b480970d13968ed7ea9cafbf7c4874
Julius Marema <juliusmarema26@gmail.com>
2026-06-17 04:21:20 +0300

    Cron job

--- index.js
+++ index.js
@@ -85,7 +85,7 @@ app.use(express.json())
 
 console.log('starting cron setup...');
 
-cron.schedule('20 4 * * *', async () => {
+cron.schedule('0 0 * * *', async () => {
 
   console.log('Cron triggered');
 
@@ -93,7 +93,7 @@ cron.schedule('20 4 * * *', async () => {
     await Ticket.destroy({
       truncate: true
     });
-    console.log('All tokens deleted successfully at 04:20');
+    console.log('All tokens deleted successfully at 00:00');
   } catch (error) {
     console.error('Error deleting tokens:', error);
   }
